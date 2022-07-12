72dfabfd0ed98a6eb53bd972cbd24e566ac1249c
Ileana Vera-Luna <ileanaveraluna@gmail.com>
2022-07-12 16:40:19 -0400

    Adding script

---
+++ loadingData.R
@@ -0,0 +1,11 @@
+#### Load in required libraries ####
+library(ggplot2)
+
+#### Load in csv file ####
+x <- 5
+
+Hero_data <- read.csv("data/heroes_information.csv")
+View(Hero_data)
+
+ggplot(data + Hero_data)
+labs(title ="title"")
